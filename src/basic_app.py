5cd0ce9a330cc08040c46f475b96450259eda8fa
phfimmers <philippefimmers@gmail.com>
2021-01-14 21:29:35 +0100

    add prediction & reply

--- src/basic_app.py
+++ src/basic_app.py
@@ -5,34 +5,31 @@ import cv2
 from flask import Flask, render_template, request, redirect, url_for, abort
 import numpy as np
 from werkzeug.utils import secure_filename
-from model.model import mole_model
+from keras.models import load_model
 import requests
 
-# load the model
-model = mole_model()
+from preprocessing.preprocess import selma_secret_sauce
 
-# download weights
-url = 'https://b0ykepubbucket.s3-eu-west-1.amazonaws.com/weights.h5'
-
+# download model
+url = 'https://b0ykepubbucket.s3-eu-west-1.amazonaws.com/64model.h5'
 r = requests.get(url, stream = True)
-
 chunk_progress = 0
-
-with open("weights.h5", "wb") as weights:
-
+with open("modelfile.h5", "wb") as modelfile:
     for chunk in r.iter_content(chunk_size = 8388608):
-
         if chunk:
-
-            weights.write(chunk)
+            modelfile.write(chunk)
             chunk_progress += 1
             print(f"Downloaded: {chunk_progress*8}MB\n")
             sys.stdout.flush()
 
+# load the model
+model = load_model('modelfile.h5')
+
+# start the app
 app = Flask(__name__, template_folder='./template')
 app.config['EXPLAIN_TEMPLATE_LOADING'] = True
-app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
-app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.gif', '.bmp']
+app.config['MAX_CONTENT_LENGTH'] = 2500 * 2500
+app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.jpeg', '.png', '.gif', '.bmp']
 app.config['UPLOAD_PATH'] = 'uploads'
 
 @app.route('/')
@@ -49,11 +46,23 @@ def upload_files():
             abort(400)
         # integrate preprocessing here
         image = cv2.imdecode(np.frombuffer(uploaded_file.read(), np.uint8), cv2.IMREAD_UNCHANGED)
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image = selma_secret_sauce(image, advanced = False, for_training = False, model_size = (64,64))
         # integrate prediction here
+        image = np.expand_dims(image, axis=0)
+        prediction = model.predict(image)
+        print(f"Predicted: {prediction[0][0]}\n")
+        sys.stdout.flush()
         # integrate reply here
-     
-    return redirect(url_for('index'))
+        if prediction[0][0] >= 0.5:
+            print("redirecting to doctor")
+            sys.stdout.flush()
+            return "Please have it checked by a doctor."
+        else:
+            print("redirecting to no doctor")
+            sys.stdout.flush()
+            return "This looks harmless"
+    
+    return redirect(url_for('upload_files'))
 
 if __name__ == "__main__":
     # You want to put the value of the env variable PORT if it exist
